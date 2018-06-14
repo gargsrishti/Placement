8a502f77b44604e42ecca54fc7608bce0e4e4a3e
gargsrishti <gargsrishti@users.noreply.github.com>
2018-06-14 15:41:06 +0530

    .

--- app/routes/student.routes.js
+++ app/routes/student.routes.js
@@ -1,18 +1,18 @@
 module.exports = (app) => {
     const students = require('../controllers/student.controller.js');
 
-    // Create a new Note
+    // Create a new Student instance
     app.post('/students', students.create);
 
-    // Retrieve all Notes
+    // Retrieve all Students
     app.get('/students', students.findAll);
 
-    // Retrieve a single Note with noteId
+    // Retrieve a single Student data with StudentId
     app.get('/students/:studentId', students.findOne);
 
-    // Update a Note with noteId
+    // Update a Student with StudentId
     app.put('/students/:studentId', students.update);
 
-    // Delete a Note with noteId
+    // Delete a Student with StudentId
     app.delete('/students/:studentId', students.delete);
 }
